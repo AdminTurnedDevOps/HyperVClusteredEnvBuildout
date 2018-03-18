713bc1532aa1db09b26a3a0fe52513e774d90a84
WindowsAdmin92 <mlevan1992@gmail.com>
2018-03-18 18:02:01 -0400

    First config of Hyper-V new cluster rolout

---
+++ HyperVandNetworkingDSC.ps1
@@ -0,0 +1,49 @@
+﻿Install-Module xPSDesiredStateConfiguration -force
+Install-Module xNetworking -Force
+
+
+Configuration HyperVandNetworkingConfig
+{
+    Param(
+        [ValidateNotNullOrEmpty()]
+        [string]$Nodename = 'localhost',
+
+        [ValidateNotNullOrEmpty()]
+        [string]$IPAddress,
+
+        [ValidateNotNullOrEmpty()]
+        [string]$InterfaceAlias,
+
+        [ValidateNotNullOrEmpty()]
+        [int]$SubnetMask,
+
+        [ValidateNotNullOrEmpty()]
+        [string]$AddressFamily
+
+
+    )
+    Import-Module PSDesiredStateConfiguration
+    Import-module xNetworking
+    Import-DscResource -Module xNetworking
+    Node $Nodename
+    {
+
+        WindowsFeature InstallHyperV
+        {
+            Ensure = 'present'
+            Name = 'Hyper-V'
+            IncludeAllSubFeature = $true
+            Credential = $Credential
+        }
+
+        xIPAddress NewIP
+        {
+            IPAddress = "192.168.1.0"
+            InterfaceAlias = "Ethernet"
+            SubnetMask = "24"
+            AddressFamily  = "IPV4"
+        }
+    }#Node
+}#Config
+
+Restart-Computer $Nodename
